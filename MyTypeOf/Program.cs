a05ef07eee0e39904d7e7f1902ebc519cd53b3cc
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-12 16:15:58 +0300

    Add project files.

---
+++ MyTypeOf/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyTypeOf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(User);
+            Console.WriteLine(type.ToString());
+        }
+    }
+}
---
+++ MyTypeOf/User.cs
@@ -0,0 +1,21 @@
+﻿namespace MyTypeOf
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public User(string n, int a)
+        {
+            Name = n;
+            Age = a;
+        }
+        public void Display()
+        {
+            System.Console.WriteLine($"Name = {Name}, Age = {Age}");
+        }
+        public int Payment(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
