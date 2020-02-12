85ea93f51e120ba88406d3a83a6ab877317c7783
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-12 16:39:01 +0300

    Get_Type_off

--- MyTypeOf/Program.cs
+++ MyTypeOf/Program.cs
@@ -6,8 +6,12 @@ namespace MyTypeOf
     {
         static void Main(string[] args)
         {
-            Type type = typeof(User);
-            Console.WriteLine(type.ToString());
+            Type type = Type.GetType("MyTypeOf.User", false, true);
+            foreach(var mi in type.GetMembers())
+            {
+                Console.WriteLine($"{mi.DeclaringType} {mi.MemberType} {mi.Name}");
+            }
+            Console.ReadKey();
         }
     }
 }
